6a25e9b1519501bef678bc43329e3e9c1ff5bdcf
Kevin Duraj <kevinduraj@gmail.com>
2014-03-18 14:33:10 -0700

    binarization output capture

--- Binarization/src/processing/Main.java
+++ Binarization/src/processing/Main.java
@@ -1,7 +1,17 @@
 package processing;
 
 import java.io.IOException;
+/*--------------------------------------------------------------------------------------------------
+Program Output:
+    1.) src/image/LennaStretch.png
+    Stretch 10th and 90th percentile - Image Output Mean = 133.19990921020508
 
+    2.) src/image/LennaMeanBin123.png
+    Binarization using mean threshold (123) - Image Output Mean = 138.29360961914062
+
+    3.) src/image/LennaOtsuBin115.png
+    Binarization using Otsu optimal threshold algorithm - Image Output Mean = 148.3531150817871
+/*------------------------------------------------------------------------------------------------*/
 public class Main {
 
     public static final int DEBUG = 2;
